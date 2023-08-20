3f1d415986dc446c47959c9c0c7570db14f86069
rexaulrana <rexaul.rana@gmail.com>
2023-08-20 21:47:38 +0600

    header section added

---
+++ js/shop.js
@@ -0,0 +1,14 @@
+// function itemInfo(data) {
+//   const itemName =
+//     data.parentNode.childNodes[5].childNodes[3].childNodes[3].innerText;
+//   console.log(itemName);
+//   const selectedItems = document.getElementById("selected-items");
+//   const count = selectedItems.childElementCount;
+//   const p = document.createElement("p");
+//   p.innerHTML = `${count} ${itemName}`;
+//   selectedItems.appendChild(p);
+
+//   //   console.log(
+//   //      data.parentNode.childNodes[5].childNodes[3].childNodes[3].innerText
+//   //   );
+// }

0431edc516827d8fc35e35a4161b5220cc39e115
rexaulrana <rexaul.rana@gmail.com>
2023-08-21 09:05:17 +0600

    Apply coupon added and total price , discount, ground total set

--- js/shop.js
+++ js/shop.js
@@ -1,14 +1,75 @@
-// function itemInfo(data) {
-//   const itemName =
-//     data.parentNode.childNodes[5].childNodes[3].childNodes[3].innerText;
-//   console.log(itemName);
-//   const selectedItems = document.getElementById("selected-items");
-//   const count = selectedItems.childElementCount;
-//   const p = document.createElement("p");
-//   p.innerHTML = `${count} ${itemName}`;
-//   selectedItems.appendChild(p);
+let total = 0;
+let discount = 0;
 
-//   //   console.log(
-//   //      data.parentNode.childNodes[5].childNodes[3].childNodes[3].innerText
-//   //   );
+function itemInfo(data) {
+  //set item
+  const itemName =
+    data.parentNode.childNodes[5].childNodes[3].childNodes[3].innerText;
+
+  const selectedItems = document.getElementById("selected-items");
+  const count = selectedItems.childElementCount;
+  const p = document.createElement("p");
+  p.innerHTML = `${count}. ${itemName}`;
+  selectedItems.appendChild(p);
+
+  //   set price
+  const priceString =
+    data.parentNode.childNodes[5].childNodes[3].childNodes[5].childNodes[0]
+      .innerText;
+
+  const price = parseFloat(priceString);
+  total = total + price;
+  const makePurchaseBtn = document.getElementById("make-purchase-btn");
+  const applyBtn = document.getElementById("apply-btn");
+
+  //   enable purchase button
+  if (total > 0) {
+    makePurchaseBtn.removeAttribute("disabled");
+  }
+
+  //   enable coupon apply button
+  if (total > 200) {
+    applyBtn.removeAttribute("disabled");
+  }
+
+  document.getElementById("total-price").innerText = total.toFixed(2);
+
+  // get coupon code  from coupon field
+
+  document.getElementById("apply-btn").addEventListener("click", function () {
+    const couponCode = document.getElementById("coupon-field").value;
+    if (couponCode === "SELL200") {
+      const percentage = (total / 200) * 100;
+
+      const discountFieldString = (document.getElementById(
+        "discount-amount"
+      ).innerText = parseFloat(percentage).toFixed(2));
+      //   const discountField = parseFloat(discountFieldString);
+      //   discountField.innerText = parseFloat(percentage);
+
+      const previousGroundTotalString =
+        document.getElementById("grand-total").innerText;
+      const groundTotalFloat = parseFloat(previousGroundTotalString);
+      const groundTotal = total - discountFieldString;
+      console.log(groundTotal);
+      document.getElementById("grand-total").innerText = groundTotal.toFixed(2);
+
+      //   Purchase
+    } else {
+      alert("invalid coupon code");
+    }
+    // const discountValue = discountField.innerText;
+    // const discountAmount = parseFloat(discountValue);
+    // discountField.innerText = percentage;
+    // console.log(discountAmount);
+    couponCode.value = "";
+  });
+}
+// get coupon code  from coupon field
+
+// function applyCoupon() {
+//   //   const applyBtn = document.getElementById("apply-btn");
+
+//   const couponCode = document.getElementById("coupon-field").value;
+
 // }
